22af9dab17c340ace8b9378f0c05ec853a53a2cc
Johnston <jcgh00@okdaily.com>
2015-05-06 13:18:15 -0700

    Created cambhei-e project as parallel dir structure instead of branch, using tortoiseSvn against gitHub

---
+++ app-cambhei-e/src/main/java/com/ok88/andydev/cambhei_e/MainActivity.java
@@ -0,0 +1,43 @@
+//5-6-15 JChoy New app project Cambhei-E by Copied from Cholly-B.
+//       Changed 5 files: app.iml, build.gradle, andydev/*/MainActivity.java, AndroidManifest.xml, and strings.xml
+//       Created as parallel dir structure instead of branch, using tortoiseSvn against gitHub
+
+package com.ok88.andydev.cambhei_e;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
